cb5cdac7790f0130fac6af805375bbadda6fa95a
Mateus Andrade <mateusandrade080@gmail.com>
2016-09-12 17:51:27 -0300

    Add division method

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,9 @@
 class Calculadora(object):
 
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
+      self.a = a
+      self.b = b
+
+    def divisao(self):
+      return self.a / self.b
+
